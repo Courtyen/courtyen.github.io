e3f57cfbb48b70d328a02e15151575d4c0b3f9fd
Courtney Liddle <31116646+Courtyen@users.noreply.github.com>
2024-05-22 20:53:13 +1000

    adding logout

--- recipe.js
+++ recipe.js
@@ -2,4 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     if (localStorage.getItem('isLoggedIn') !== 'true') {
         window.location.href = 'index.html';
     }
+
+    document.getElementById('logoutButton').addEventListener('click', function() {
+        localStorage.removeItem('isLoggedIn');
+        window.location.href = 'index.html';
+    });
 });
